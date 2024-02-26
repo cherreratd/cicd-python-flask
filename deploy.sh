2549c867c9761946a7be1ce63fccf2d6d44574a3
cherreratd <94548042+cherreratd@users.noreply.github.com>
2024-02-26 15:47:38 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ sudo rm -rf /home/ubuntu/www/
 
 echo "creating app folder"
 sudo mkdir -p /home/ubuntu/www/app
+sudo chown -R ubuntu:www-data /var/www
+sudo chmod -R 750 /var/www
 
 echo "moving files to app folder"
 sudo mv  * /home/ubuntu/www/app
